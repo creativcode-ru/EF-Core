c4ab28bdbe58f7ce4644836bb8d86f67408f8ced
Алексей <burdykin@live.ru>
2020-02-05 15:33:12 +0300

    Консольное приложение EF Get Started

---
+++ EFGetStarted/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+
+//Начало работы с EF Core https://docs.microsoft.com/ru-ru/ef/core/get-started/?tabs=visual-studio
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var db = new BloggingContext())
+            {
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post
+                    {
+                        Title = "Hello World",
+                        Content = "I wrote an app using EF Core!"
+                    });
+                db.SaveChanges();
+
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
+        }
+    }
+}

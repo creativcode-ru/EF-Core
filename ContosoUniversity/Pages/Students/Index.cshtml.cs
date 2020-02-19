fb7509a10d10cc4497805f2671f575020e157dd0
Алексей <burdykin@live.ru>
2020-02-19 17:51:06 +0300

    Сортировка, фильтрация, разбиение на страницы

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 
@@ -19,11 +20,68 @@ namespace ContosoUniversity
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; } //сохраняет значение для фильтрации
+        public string CurrentSort { get; set; } //сохраняет праметр сортировки
 
-        public async Task OnGetAsync()
+        //public IList<Student> Students { get;set; } //имя сойства Student заменяется на Students
+        public PaginatedList<Student> Students { get; set; }
+
+        /* Метод OnGetAsync принимает параметр sortOrder из строки запроса в URL-адресе. 
+         * URL-адрес (включая строку запроса) формируется вспомогательной функцией тегов привязки.
+         * 
+         * Параметр sortOrder имеет значение "Name" или "Date". 
+         * После параметра sortOrder может стоять "_desc", чтобы указать порядок по убыванию. 
+         * По умолчанию используется порядок сортировки по возрастанию
+         */
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            Student = await _context.Students.ToListAsync();
+            CurrentSort = sortOrder;
+            //параметры сортировки из строки запроса
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1; //сбрасывает индекс страницы в значение 1 при получении новой строки поиска;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+           
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+            }
+
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            //Students = await studentsIQ.AsNoTracking().ToListAsync(); //здесь происходит обращение к БД
+            int pageSize = 3;
+            Students = await PaginatedList<Student>.CreateAsync(
+                studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
---
+++ ContosoUniversity/PaginatedList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+
+        
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        // Для включения и отключения кнопок перелистывания страниц Previous (Назад) и Next (Далее) используются свойства HasPreviousPage и HasNextPage.
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        //метод CreateAsync принимает размер и номер страницы и вызывает соответствующие методы Skip и Take объекта IQueryable. 
+        public static async Task<PaginatedList<T>> CreateAsync(
+            IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip(
+                (pageIndex - 1) * pageSize)
+                .Take(pageSize).ToListAsync(); //Метод ToListAsync объекта IQueryable при вызове возвращает список, содержащий только запрошенную страницу.
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+        /* Метод CreateAsync используется для создания PaginatedList<T>. 
+         * Конструктор не позволяет создать объект PaginatedList<T>, так как конструкторы не могут выполнять асинхронный код.
+         */
+    }
+}

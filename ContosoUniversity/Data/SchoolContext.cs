8630cd2ae8ae912597e0834f208303e35692722b
Алексей <burdykin@live.ru>
2020-02-21 17:16:49 +0300

    Создание сложной модели данных

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -28,14 +28,26 @@ namespace ContosoUniversity.Data
          * Так как средство формирования шаблонов создало DBSet Student, 
          * в этом шаге его имя меняется на имя во множественном числе: Students.
          */
-
         //Чтобы код Razor Pages соответствовал новому имени DBSet, измените _context.Student на _context.Students в рамках всего
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
             modelBuilder.Entity<Student>().ToTable("Student");
+
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID }); //код добавляет новые сущности и настраивает составной первичный ключ сущности CourseAssignment.
         }
     }
 }

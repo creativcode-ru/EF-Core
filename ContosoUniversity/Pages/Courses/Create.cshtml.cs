f094c85d0df8494036e67ac9f5df28e0537d76e3
Алексей <burdykin@live.ru>
2020-02-21 19:12:30 +0300

    Формирование шаблона для страниц преподавателей

---
+++ ContosoUniversity/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public CreateModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        /* Шаблон
+        public IActionResult OnGet()
+        {
+        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            return Page();
+        }
+        */
+
+        public IList<Course> Courses { get; set; }
+        public async Task OnGetAsync()
+        {
+            Courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking() //AsNoTracking повышает производительность, так как возвращаемые сущности не отслеживаются. Отслеживать сущности не нужно, так как они не изменяются в текущем контексте.
+                .ToListAsync();
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Courses.Add(Course);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ContosoUniversity/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        
+        public IList<Course> Courses { get;set; }
+        public async Task OnGetAsync()
+        {
+            Courses = await _context.Courses
+                .Include(c => c.Department).ToListAsync();
+
+
+        }
+        /* ШАБЛОН*/
+
+
+        /*
+    public IList<CourseViewModel> CourseVM { get; set; }
+
+
+    public async Task OnGetAsync()
+    {
+        CourseVM = await _context.Courses
+                .Select(p => new CourseViewModel
+                {
+                    CourseID = p.CourseID,
+                    Title = p.Title,
+                    Credits = p.Credits,
+                    DepartmentName = p.Department.Name
+                }).ToListAsync();
+    }
+
+    public class CourseViewModel
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public string DepartmentName { get; set; }
+    }
+    */
+
+    }
+}
